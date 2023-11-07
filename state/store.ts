e5f1866e95622223f803c8200b0574bcf869e736
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-11-06 23:47:49 -0500

    add zustand store

---
+++ state/store.ts
@@ -0,0 +1,22 @@
+"use client"
+// https://docs.pmnd.rs/zustand/guides/typescript
+import { create } from "zustand"
+import { fallbackLng } from "@/app/i18n/settings"
+
+type State = {
+    lng?: string
+    theme?: string
+}
+
+type Actions = {
+    setLng(lng: string): void
+    setTheme(theme: string): void
+}
+
+export const useStore = create<State & Actions>()(set => ({
+    lng: fallbackLng,
+    theme: 'dark',
+    setLng: (lng: string) => set((state) => ({ ...state, lng })),
+    setTheme: (theme: string) => set((state) => ({ ...state, theme }))
+}))
+
