3b92776dbe83b3e7a87bea9d6817a950c41c6c20
Massick <4ndyraul@gmail.com>
2024-11-09 00:15:25 -0300

    add /about page

---
+++ app/[lng]/about/layout.tsx
@@ -0,0 +1,33 @@
+import { dir } from 'i18next'
+import { languages } from '../../i18n/settings'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'About',
+    description: 'About me',
+}
+
+export async function generateStaticParams() {
+    return languages.map((lng) => ({ lng }))
+}
+
+export default function RootLayout({
+    children,
+    params: {
+        lng
+    }
+}: {
+    children: React.ReactNode,
+    params: {
+        lng: string
+    }
+}) {
+    return (
+        <html lang={lng} dir={dir(lng)}>
+            <head />
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ app/[lng]/about/page.tsx
@@ -0,0 +1,31 @@
+import { useTranslation } from '../../i18n'
+import Helper from '@/components/Helper'
+import Image from 'next/image'
+import Link from 'next/link'
+import profile from '@/public/images/profile.jpg'
+
+export default async function Page({ params: { lng } }: { params: { lng: string } }) {
+    const { t } = await useTranslation(lng)
+
+    return (
+        <main className="flex flex-col items-center justify-center w-full h-screen gap-2">
+            <Helper lng={lng} />
+            <h1 className="mt-4 text-lg uppercase lg:text-2xl font-rammetto text-primario dark:text-secundario">
+                <span>{t("who_am_I")}</span>
+            </h1>
+            <Image
+                className="rounded-full"
+                src={profile}
+                width={160}
+                height={160}
+                alt={t("Foto de perfil")}
+            />
+            <p className="px-4 pt-8 text-xl font-semibold font-montserrat md:w-2/3 md:pt-4 md:text-2xl max-w-prose">
+                {t("about_bio")}
+            </p>
+            <Link href="/" className='font-thin font-mono underline'>
+                {t("back")}
+            </Link>
+        </main>
+    )
+}
