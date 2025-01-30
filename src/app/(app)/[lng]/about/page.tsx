179054afae5fb6bd14337158e0eac8cc3f9b78fd
Massick <4ndyraul@gmail.com>
2025-01-29 21:57:25 -0300

    improve about page

--- src/app/(app)/[lng]/about/page.tsx
+++ src/app/(app)/[lng]/about/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
     const t = useTranslations()
 
     return (
-        <main className="flex flex-col items-center justify-start w-full h-screen gap-2">
+        <main className="flex flex-col items-center justify-start w-full min-h-screen gap-2">
             <Navbar />
             <h1 className="mt-4 text-lg uppercase lg:text-2xl font-display text-primary">
                 <span>{t("who_am_I")}</span>
@@ -42,6 +42,9 @@ export default function Page() {
             </div>
             <p className="px-4 pt-8 text-xl font-semibold font-display md:w-2/3 md:pt-4 md:text-2xl max-w-prose text-pretty text-secondary">
                 {t("about_bio")}
+            </p>
+            <p className="px-4 pt-8 text-xl font-semibold font-display md:w-2/3 md:pt-4 md:text-2xl max-w-prose text-pretty text-secondary">
+                {t("extended_bio")}
             </p>
             <Link href="/" className='font-thin font-body underline-animation text-primary'>
                 {t("back")}
