82e0826eb5c9b01ba9620b543ea71e2c5ba81449
Massick <4ndyraul@gmail.com>
2025-02-06 22:50:57 -0300

    fix block height

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,6 +1,6 @@
 export const Block: React.FC<React.PropsWithChildren> = ({ children }) => {
     return (
-        <div className="flex flex-col items-center justify-center w-full h-screen font-body overflow-hidden">
+        <div className="flex flex-col items-center justify-center w-full min-h-screen font-body overflow-hidden">
             {children}
         </div>
     )
