fdd6f02ee0b6bc19e200a144390d1902c029ec6d
Massick <4ndyraul@gmail.com>
2025-02-06 22:52:22 -0300

    add projects and footer to home page

--- src/app/(app)/[lng]/page.tsx
+++ src/app/(app)/[lng]/page.tsx
@@ -3,17 +3,34 @@ import Menu from '@/components/Menu'
 import LettersAnimation from '@/components/LettersAnimation'
 import { useTranslations } from 'next-intl'
 import Hacker from '@/components/icons/Hacker'
+import { Block } from '@/components/Block'
+import { ProjectList } from '@/components/ProjectList'
+import { GoUp } from '@/components/GoUp'
+import { Footer } from '@/components/Footer'
+import { Link } from '@/components/AnimatedLink'
 
 export default function Page() {
     const t = useTranslations()
     const title = t("titulo_portada")
 
     return (
-        <main className="flex flex-col items-center justify-center w-full h-screen font-body overflow-x-hidden">
-            <Canvas r={250} g={250} b={250} text={title} />
-            <Menu t={t} />
-            <LettersAnimation title={title} />
-            <Hacker className="absolute bottom-0 w-full opacity-20 -z-10" />
+        <main className="flex flex-col items-center justify-center w-full min-h-screen font-body overflow-x-hidden">
+            <Block>
+                <Canvas r={250} g={250} b={250} text={title} />
+                <Menu t={t} />
+                <LettersAnimation title={title} />
+                <Hacker className="absolute bottom-0 w-full opacity-20 -z-10" />
+            </Block>
+            <Block>
+                <Link href="/projects">
+                    <h1 className='text-4xl text-primary font-body mt-4 mb-8 underline-animation'>
+                        {t("projects")}
+                    </h1>
+                </Link>
+                <ProjectList favoritesOnly limit={6} />
+            </Block>
+            <Footer />
+            <GoUp />
         </main>
     )
 }
