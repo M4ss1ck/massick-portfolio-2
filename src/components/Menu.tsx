27ca7b197d40483df7188b25b3f6a3f71ca1cd84
Massick <4ndyraul@gmail.com>
2025-02-06 21:47:11 -0300

    remove commented out code

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,7 +5,6 @@ import { Link } from './AnimatedLink'
 export default function Menu({ t }: { t: (value: string) => string }) {
     return (
         <aside className="flex flex-row py-1 px-4 md:flex-col justify-between items-center md:items-start absolute w-full md:w-fit left-0 top-0 md:top-[60vh] z-20 rounded-lg lg:transition text-lg text-primary">
-            {/* <DarkToggle /> */}
             <h2 className="p-1 sm:p-4 text-sm sm:block lg:text-2xl">
                 <Link href="/projects" className="underline-animation">
                     {t("projects")}
