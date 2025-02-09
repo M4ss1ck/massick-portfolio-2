bd90a78051a0f0952e1848091866fd82fc6522a5
Massick <4ndyraul@gmail.com>
2025-02-09 17:23:14 -0300

    Block component rework

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,7 +1,14 @@
-export const Block: React.FC<React.PropsWithChildren> = ({ children }) => {
+export const Block: React.FC<React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>> = ({
+    children,
+    className,
+    ...props
+}) => {
     return (
-        <div className="flex flex-col items-center justify-center w-full min-h-screen font-body overflow-hidden">
+        <div
+            className={`flex flex-col items-center justify-center w-full min-h-screen font-body overflow-hidden ${className || ""}`}
+            {...props}
+        >
             {children}
         </div>
-    )
-}
+    );
+};
