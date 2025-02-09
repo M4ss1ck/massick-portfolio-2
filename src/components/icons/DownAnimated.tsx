4826e619cff416efd096360d3333e5b08019e8fe
Massick <4ndyraul@gmail.com>
2025-02-09 20:18:27 -0300

    add down arrow icon

---
+++ src/components/icons/DownAnimated.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function Down(props: SVGProps<SVGSVGElement>) {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width={48} height={48} viewBox="0 0 24 24" {...props}>
+        <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}>
+            <path fill="currentColor" fillOpacity={0} strokeDasharray={20} strokeDashoffset={20} d="M12 4h2v6h2.5l-4.5 4.5M12 4h-2v6h-2.5l4.5 4.5">
+                <animate attributeName="d" begin="0.5s" dur="1.5s" repeatCount="indefinite" values="M12 4h2v6h2.5l-4.5 4.5M12 4h-2v6h-2.5l4.5 4.5;M12 4h2v3h2.5l-4.5 4.5M12 4h-2v3h-2.5l4.5 4.5;M12 4h2v6h2.5l-4.5 4.5M12 4h-2v6h-2.5l4.5 4.5"></animate>
+                <animate fill="freeze" attributeName="fill-opacity" begin="0.7s" dur="0.5s" values="0;1"></animate>
+                <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="20;0"></animate>
+            </path>
+        </g>
+    </svg>);
+}
