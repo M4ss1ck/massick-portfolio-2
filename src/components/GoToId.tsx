2894ddb2fb91fb18fe4e9bb5357e0c29774db2b8
Massick <4ndyraul@gmail.com>
2025-02-14 21:25:23 -0300

    hide arrow button on scroll

--- src/components/GoToId.tsx
+++ src/components/GoToId.tsx
@@ -1,15 +1,32 @@
 "use client"
+import { useState, useEffect } from 'react'
 import { Down } from '@/components/icons/DownAnimated'
 
 export const GoToId = ({ id }: { id: string }) => {
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setVisible(window.scrollY < 150)
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     const goToProjects = (id: string) => {
         const targetElement = document.getElementById(id)
         if (targetElement) {
             targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
         }
     }
+
     return (
-        <button className='absolute bottom-0 h-8 w-8 text-primary hover:text-other' onClick={() => goToProjects(id)}>
+        <button className={`absolute bottom-0 h-8 w-8 text-primary hover:text-other transition-all duration-300 ease-in-out ${visible ? 'visible' : 'invisible'}`} onClick={() => goToProjects(id)}>
             <Down />
         </button>
     )
