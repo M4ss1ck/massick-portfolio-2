9be75cfe31aed70e4cf6989f544af3ba7749215c
Massick <4ndyraul@gmail.com>
2025-02-15 17:44:57 -0300

    modify arrow down button

--- src/components/icons/DownAnimated.tsx
+++ src/components/icons/DownAnimated.tsx
@@ -2,13 +2,60 @@ import React from 'react';
 import type { SVGProps } from 'react';
 
 export function Down(props: SVGProps<SVGSVGElement>) {
-    return (<svg xmlns="http://www.w3.org/2000/svg" width={48} height={48} viewBox="0 0 24 24" {...props}>
-        <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}>
-            <path fill="currentColor" fillOpacity={0} strokeDasharray={20} strokeDashoffset={20} d="M12 4h2v6h2.5l-4.5 4.5M12 4h-2v6h-2.5l4.5 4.5">
-                <animate attributeName="d" begin="0.5s" dur="1.5s" repeatCount="indefinite" values="M12 4h2v6h2.5l-4.5 4.5M12 4h-2v6h-2.5l4.5 4.5;M12 4h2v3h2.5l-4.5 4.5M12 4h-2v3h-2.5l4.5 4.5;M12 4h2v6h2.5l-4.5 4.5M12 4h-2v6h-2.5l4.5 4.5"></animate>
-                <animate fill="freeze" attributeName="fill-opacity" begin="0.7s" dur="0.5s" values="0;1"></animate>
-                <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="20;0"></animate>
-            </path>
-        </g>
-    </svg>);
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={48}
+            height={48}
+            viewBox="0 0 24 24"
+            fill="none"
+            {...props}
+        >
+            <defs>
+                <path
+                    id="arrowhead"
+                    d="M-8 5L0 13 8 5"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+            </defs>
+            <g transform="translate(12 0)">
+                <use href="#arrowhead">
+                    <animateTransform
+                        attributeName="transform"
+                        type="scale"
+                        values="1;1.1;1"
+                        dur="1.2s"
+                        begin="0s"
+                        repeatCount="indefinite"
+                    />
+                    <animate
+                        attributeName="opacity"
+                        values="1;0.6;1"
+                        dur="1.2s"
+                        repeatCount="indefinite"
+                    />
+                </use>
+            </g>
+            <g transform="translate(12 8)">
+                <use href="#arrowhead">
+                    <animateTransform
+                        attributeName="transform"
+                        type="scale"
+                        values="1;1.1;1"
+                        dur="1.2s"
+                        repeatCount="indefinite"
+                    />
+                    <animate
+                        attributeName="opacity"
+                        values="0.6;1;0.6"
+                        dur="1.2s"
+                        repeatCount="indefinite"
+                    />
+                </use>
+            </g>
+        </svg>
+    );
 }
