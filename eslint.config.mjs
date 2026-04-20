50f09d45ee9ac2d9bb03f36d131b9925395704ba
Andy Palmero <4ndyraul@gmail.com>
2026-04-20 15:50:38 -0400

    minor prettier adjustments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
-export default defineConfig([{
-    extends: [...nextCoreWebVitals, ...nextTypescript],
-}]);
+export default defineConfig([
+    {
+        extends: [...nextCoreWebVitals, ...nextTypescript],
+    },
+]);
